06cf7ed62f2ca79e78d92ddc4ffe6069e5db04e5
randydeploys <randydeploys@gmail.com>
2025-11-15 00:00:01 +0100

    feat(security): implement role hierarchy and access control

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin', name: 'admin_')]
+#[IsGranted('ROLE_ADMIN')]
+final class AdminController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+}
